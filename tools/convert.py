c805add72814df38fc314cc63e595fb0138c2d5a
Gary Bishop <gary.bishop.unc@gmail.com>
2019-11-22 11:48:09 -0500

    Reduce the number of frames in Knight.

--- tools/convert.py
+++ tools/convert.py
@@ -8,6 +8,7 @@ import math
 root = "/data/assets/Isometric Tower Defense/1. Characters"
 character = "Knight"
 nframes = 30
+framestep = 3
 
 folder = osp.join(root, character)
 
@@ -23,18 +24,21 @@ patterns = {
 # get the max width and height
 mw = 0
 mh = 0
+images = {}
 for direction in directions:
+    images[direction] = []
     pattern = patterns[direction]
-    for j in range(nframes):
+    for j in range(0, nframes, framestep):
         path = osp.join(folder, pattern % (character, j))
         im = Image.open(path).convert("RGBA")
+        images[direction].append(im)
         w, h = im.size
         mw = max(mw, w)
         mh = max(mh, h)
 
 
 def scale(d):
-    return int(math.ceil(3 * d / 8))
+    return int(math.ceil(3 * d // 8))
 
 
 # scale them
@@ -44,17 +48,16 @@ mh = scale(mh)
 
 # create the result image
 result = Image.new(
-    mode="RGBA", size=(mw * nframes, mh * len(directions)), color=(0, 0, 0, 0)
+    mode="RGBA",
+    size=(mw * (nframes // framestep), mh * len(directions)),
+    color=(0, 0, 0, 0),
 )
 
 
 # revisit the images pasting them and building the datastructure
 frames = []
 for i, direction in enumerate(directions):
-    pattern = patterns[direction]
-    for j in range(nframes):
-        path = osp.join(folder, pattern % (character, j))
-        im = Image.open(path).convert("RGBA")
+    for j, im in enumerate(images[direction]):
         w, h = im.size
         w = scale(w)
         h = scale(h)
